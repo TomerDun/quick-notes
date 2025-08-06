af1f2813d1f2740da97772e3b3a0e6c5a9673dc6
TomerDun <tomer@dunsky.name>
2025-08-06 13:57:08 +0300

    Added basic structure

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="outside-container">
+      
+      <div className="input-container">
+        
+      </div>
+
+      <div className="notes-container">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
